21bc7dc73b0306ca37711976e9fc73a7663553ef
Dilusha <sachinigalhena98@gmail.com>
2026-03-22 15:33:50 +0530

    Update event.php and header.php

--- templates/header.php
+++ templates/header.php
@@ -13,7 +13,7 @@ $pageTitleMap = [
 ];
 $currentPageTitle = $pageTitleMap[$page] ?? ucfirst($page);
 $currentAction = $_GET['action'] ?? 'index';
-$showReportButton = in_array($page, ['retail', 'event', 'products', 'suppliers', 'employees']) && $currentAction === 'index';
+$showReportButton = in_array($page, ['retail', 'products', 'suppliers', 'employees']) && $currentAction === 'index';
 ?>
 <!DOCTYPE html>
 <html lang="en">
